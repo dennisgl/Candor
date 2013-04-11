5defbf77bd47668241d5cd1cef948522ded07708
Geng-Yu Lin <pioneerlike@gmail.com>
2013-04-12 07:44:53 +0800

    Initial Commit

---
+++ Candor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Candor
+//
+//  Created by Denny on 13/4/12.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
